934a44bbee7ae14c837248c2133874bf91e4580e
CreationWalker <basharin.artem2008@mail.ru>
2025-08-13 13:58:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function f(word) {
 //let x = Math.random()*10**16
 let x = parseInt(word, 36)
 let exp = parseInt(Math.pow(x, 1/x).toExponential(100).replaceAll('0', '').replace('e+', '').replaceAll('1.', ''))
-let b36 = exp.toString(36).replaceAll('0','')
+let b36 = exp.toString(36).replaceAll('00','')
 return b36
 }
