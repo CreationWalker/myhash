2d35754617f2198c3234f6e531b7311c36a9ef31
CreationWalker <basharin.artem2008@mail.ru>
2025-08-14 06:57:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function cipher(word) {
-//let x = Math.random()*10**16
 let x = parseInt(word, 36)
 let exp = parseInt(Math.pow(x, 1/x).toExponential(64).replaceAll('0', '').replace('e+', '').replaceAll('1.', ''))
 let b16 = exp.toString(16)
