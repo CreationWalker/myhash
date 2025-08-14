864da14d788d9a404297a74266b40cfc9b788bf6
CreationWalker <basharin.artem2008@mail.ru>
2025-08-14 08:02:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function newtonsMethod(x, initialGuess = 0.6, tolerance = 1e-12, maxIterations =
 }
 function decipher(x) {
 x = BigInt(base10(BigInt(x)))
-let result = parseInt(newtonsMethod(base10(x), 0.6).toExponential(-64));
+let result = BigInt(newtonsMethod(base10(x), 0.6)).toExponential(-64));
 console.log(`Solution: b = ${result}`);
 
 // Convert to base36
