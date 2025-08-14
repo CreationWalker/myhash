e6016f2bee8a037cbf3491083599ef0698cccd86
CreationWalker <basharin.artem2008@mail.ru>
2025-08-14 06:53:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function cipher(word) {
 //let x = Math.random()*10**16
 let x = parseInt(word, 36)
 let exp = parseInt(Math.pow(x, 1/x).toExponential(100).replaceAll('0', '').replace('e+', '').replaceAll('1.', ''))
-let b16 = exp.toString(16).replaceAll('00','')
+let b16 = exp.toString(16)
 return b16
 }
 function gen() {
 let x = Math.random()*10**16
 let exp = parseInt(Math.pow(x, 1/x).toExponential(100).replaceAll('0', '').replace('e+', '').replaceAll('1.', ''))
-let b16 = exp.toString(16).replaceAll('00','')
+let b16 = exp.toString(16)
 return b16
 }
 function base16(word) {
