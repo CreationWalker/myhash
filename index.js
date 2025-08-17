27a800b472550ae7facf12beb14a402d6af0c0e4
CreationWalker <basharin.artem2008@mail.ru>
2025-08-17 16:37:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ const BigNumber = require('bignumber.js');
 function toHex(decimalStr) {
     return new BigNumber(decimalStr).toString(16);
 }
-
-// Парсинг HEX
-function fromHex(hexStr) {
-    return new BigNumber(hexStr, 16).toString();
-}
-
 function cipher(word) {
- 
  let x = parseInt(word, 36)
  exp = BigInt(parseInt(Math.pow(x, 1/x).toExponential(64).split('.')[1])+Math.pow(x, 1/x).toExponential(64).split('.')[0]*10**64)
-
  return toHex(exp)
 }
 function gen() {
@@ -24,42 +16,32 @@ function gen() {
 function newtonSolve(xHex, tolerance = 1e-12, maxIter = 1000) {
   // Convert hex string to BigInt
   const xBigInt = BigInt('0x' + xHex);
-
   // Convert BigInt to Number (may lose precision if very large)
   const x = Number(xBigInt);
   if (!isFinite(x)) {
     throw new Error('x is too large to convert to Number safely');
   }
-
   // Define f(b) = x^b - b
   // and f'(b) = x^b * ln(x) - 1
   const lnX = Math.log(x);
-
   // Initial guess for b
   let b = 1.0;
-
   for (let i = 0; i < maxIter; i++) {
     const xPowB = Math.pow(x, b);
     const f = xPowB - b;
     const fPrime = xPowB * lnX - 1;
-
     if (Math.abs(fPrime) < 1e-15) {
       throw new Error('Derivative too small, no convergence');
     }
-
     const bNext = b - f / fPrime;
-
     if (Math.abs(bNext - b) < tolerance) {
       b = bNext;
       break;
     }
-
     b = bNext;
   }
-
   // Convert b to BigInt by rounding
   const bBigInt = BigInt(Math.round(b));
-
   // Convert BigInt to base36 string
   return bBigInt.toString(36);
 }
