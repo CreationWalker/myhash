9bfe201266661f3eb96acdda063e2ff4365a027f
CreationWalker <basharin.artem2008@mail.ru>
2025-09-19 22:13:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function cipher(word) {
  let x = parseInt(word, 36)
  exp0 = BigInt(parseInt(Math.pow(x, 1/x).toExponential(64).split('.')[1])).toString(16)
- exp = exp0.replaceAll(00, '')
+ exp = exp0.split('00')[0]
  return exp
 }
 function gen() {
  let x = Math.random()*10**16
  exp0 = BigInt(parseInt(Math.pow(x, 1/x).toExponential(64).split('.')[1])).toString(16)
- exp = exp0.replaceAll(00, '')
+ exp = exp0.split('00')[0]
  return exp
 }
